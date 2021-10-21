cac90142b1b0246f2ec0dcc5b124bedb99f3816b
Paul Ladd <vlendle@gmail.com>
2021-10-21 12:03:50 -0500

    Fix bug with printing current time as string

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const player = require('play-sound')(opts = {});
 
 const remindMe = () => {
   player.play('win.wav');
-  console.log(`Reminder ping at ${Date.now().toLocaleTimeString()}`)
+  console.log(`Reminder ping at ${Date()}`)
 };
 
 console.log('Running reminder');
